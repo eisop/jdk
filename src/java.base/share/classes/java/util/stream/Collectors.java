07392cc8f1d95abf1876c61af576af083c0ba6f1
Chris Povirk <cpovirk@google.com>
2022-05-03 14:01:49 -0400

    Add `@Nullable` import.

--- src/java.base/share/classes/java/util/stream/Collectors.java
+++ src/java.base/share/classes/java/util/stream/Collectors.java
@@ -24,6 +24,7 @@
  */
 package java.util.stream;
 
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.checkerframework.dataflow.qual.Pure;
 import org.checkerframework.dataflow.qual.SideEffectFree;
 import org.checkerframework.framework.qual.AnnotatedFor;
