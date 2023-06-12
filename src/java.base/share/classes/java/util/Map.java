e584745867189d021756f69210f8c6f4d71d6e9d
Chris Povirk <cpovirk@google.com>
2023-06-12 11:30:12 -0400

    Use `@PolyNull` in `ConcurrentMap.getOrDefault` (but still not plain `Map`).

--- src/java.base/share/classes/java/util/Map.java
+++ src/java.base/share/classes/java/util/Map.java
@@ -707,6 +707,8 @@ public interface Map<K, V> {
      * @since 1.8
      */
     @Pure
+    @CFComment("nullness: We can't use @PolyNull because the output can be null even when"
+        + " defaultValue is not (namely, when the map contains a null value)")
     default V getOrDefault(@GuardSatisfied @UnknownSignedness Object key, V defaultValue) {
         V v;
         return (((v = get(key)) != null) || containsKey(key))
