f3391dc23b6c59a02bf897b1b7bfe8ef80164228
Aosen Xiong <aosen.xiong@uwaterloo.ca>
2025-05-07 18:04:08 -0400

    Fix: Remove type parameter upperbound for `ConcurrentHashMap`

--- src/java.base/share/classes/java/util/concurrent/ConcurrentHashMap.java
+++ src/java.base/share/classes/java/util/concurrent/ConcurrentHashMap.java
@@ -279,7 +279,7 @@ import jdk.internal.misc.Unsafe;
  * @param <V> the type of mapped values
  */
 @AnnotatedFor({"nullness"})
-public class ConcurrentHashMap<K extends @NonNull Object,V extends @NonNull Object> extends AbstractMap<K,V>
+public class ConcurrentHashMap<K,V> extends AbstractMap<K,V>
     implements ConcurrentMap<K,V>, Serializable {
     private static final long serialVersionUID = 7249069246763182397L;
 
