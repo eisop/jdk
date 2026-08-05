b41175922f98c9d10e69d6d589811d3986b0286e
Aosen Xiong <aosenxiong@gmail.com>
2026-08-05 18:35:05 -0400

    Apply suggestion from @aosen-xiong

--- src/java.base/share/classes/org/checkerframework/checker/initialization/qual/EnsuresInitialized.java
+++ src/java.base/share/classes/org/checkerframework/checker/initialization/qual/EnsuresInitialized.java
@@ -27,10 +27,11 @@ import java.lang.annotation.Target;
 @InheritedAnnotation
 @Repeatable(EnsuresInitialized.List.class)
 public @interface EnsuresInitialized {
-    /**
+     /**
      * The Java expressions that are initialized upon successful method termination.
      *
      * @return the Java expressions that are initialized
+     * @checker_framework.manual #java-expressions-as-arguments Syntax of Java expressions
      */
     String[] value();
 
